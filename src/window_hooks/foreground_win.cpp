d0b61c5a459738761791e2f14aece73da9e8fdd8
cengels <cengels@tuta.io>
2020-10-17 19:32:24 +0200

    Fix windows window hooks

--- src/window_hooks/foreground_win.cpp
+++ src/window_hooks/foreground_win.cpp
@@ -2,41 +2,42 @@
 #include <iostream>
 #include <string>
 #include <functional>
-#include "window_hooks.h"
 #include <psapi.h>
-HANDLE g_hTerminateEvent;
-MSG msg;
+#include "window_hooks.h"
+#include "../tracker.h"
 
-void CALLBACK WinEventProcCallback(HWINEVENTHOOK hWinEventHook, DWORD dwEvent, HWND hwnd, LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime)
+HANDLE termination_event;
+MSG last_message;
+DWORD D_MAX_PATH = MAX_PATH;
+
+void CALLBACK window_event_callback(HWINEVENTHOOK hWinEventHook, DWORD dwEvent, HWND hwnd, LONG idObject, LONG idChild, DWORD dwEventThread, DWORD dwmsEventTime)
 {
      if (dwEvent == EVENT_SYSTEM_FOREGROUND) {
-         TCHAR buffer[MAX_PATH] = { 0 };
-         DWORD dwProcId = 0;
-         //HMODULE module;
-         DWORD value = MAX_PATH;
+         TCHAR process_name[MAX_PATH] = { 0 };
+         DWORD process_id = 0;
 
-         GetWindowThreadProcessId(hwnd, &dwProcId);
+         GetWindowThreadProcessId(hwnd, &process_id);
 
-         HANDLE hProc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, dwProcId);
+         HANDLE process_handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, process_id);
 
-         if (QueryFullProcessImageName(hProc, 0, buffer, &value)) {
-             std::cout << "New foreground window is " << buffer << std::endl;
+         if (QueryFullProcessImageName(process_handle, 0, process_name, &D_MAX_PATH)) {
+             tracker::handle_new_foreground_process(process_name);
          } else {
              std::cout << "Could not retrieve process title. Error code: " << GetLastError() << std::endl;
          }
 
-         CloseHandle(hProc);
+         CloseHandle(process_handle);
      }
 }
 
-BOOL WINAPI ConsoleCtrlHandler(DWORD dwCtrlType)
+BOOL WINAPI console_ctrl_handler(DWORD dwCtrlType)
 {
     if (dwCtrlType == CTRL_C_EVENT ||
         dwCtrlType == CTRL_BREAK_EVENT ||
         dwCtrlType == CTRL_CLOSE_EVENT)
     {
         std::cout << "Terminating tracker..." << std::endl;
-        SetEvent(g_hTerminateEvent);
+        SetEvent(termination_event);
 
         PostQuitMessage(0);
     }
@@ -49,17 +50,17 @@ void window_hooks::register_foreground_window_listener(const std::function<void(
     std::cout << "Now tracking..." << std::endl;
     SetWinEventHook(EVENT_SYSTEM_FOREGROUND,
         EVENT_SYSTEM_FOREGROUND, NULL,
-        &WinEventProcCallback,
+        &window_event_callback,
         0, 0,
         WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
 
     // The application should stop anyway, but just in case it doesn't,
     // here's a handler that forces it to close.
-    g_hTerminateEvent = ::CreateEvent(NULL, FALSE, FALSE, NULL);
-    SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE);
+    termination_event = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+    SetConsoleCtrlHandler(console_ctrl_handler, TRUE);
 
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    while (GetMessage(&last_message, NULL, 0, 0) > 0) {
+        TranslateMessage(&last_message);
+        DispatchMessage(&last_message);
     }
 }
