33d1dbc23efdbc4bbb4c2b2a440472c5e6496939
cengels <cengels@tuta.io>
2020-10-22 22:04:31 +0200

    Add functions to prepare/restore/accept changes

--- src/files.cpp
+++ src/files.cpp
@@ -2,39 +2,41 @@
 #include "files.h"
 #include "rang.hpp"
 
+namespace fs = std::experimental::filesystem;
+
 namespace {
     constexpr char ENV_VARIABLE[] = "DLOG_PATH";
-    std::optional<std::experimental::filesystem::path> m_dlog_dir;
+    std::optional<fs::path> m_dlog_dir;
 }
 
 #ifdef IS_WINDOWS
-static const const std::experimental::filesystem::path default_path()
+static const const fs::path default_path()
 {
     const char* app_data_env_variable = getenv("AppData");
 
     if (app_data_env_variable == nullptr) {
         std::cerr << rang::fg::red << "Error: Could not locate AppData directory. This is likely not your fault.\n"
                      "To fix this problem, try defining your own config path as the environment variable DLOG_PATH." << rang::fg::reset << std::endl;
-        return std::experimental::filesystem::path();
+        return fs::path();
     }
 
-    std::experimental::filesystem::path app_data_path(app_data_env_variable);
+    fs::path app_data_path(app_data_env_variable);
     app_data_path.append("Roaming");
     app_data_path.append("dlog");
 
     std::error_code error;
-    if (!std::experimental::filesystem::create_directory(app_data_path, error) && error) {
+    if (!fs::create_directory(app_data_path, error) && error) {
         std::cerr << rang::fg::red << "Error: Could not create config directory.\n"
                      "File system reported: " << error.message() << " (" << error.value() << ")" << rang::fg::reset << std::endl;
-        return std::experimental::filesystem::path();
+        return fs::path();
     }
 
     return app_data_path;
 }
 #else
-static const std::experimental::filesystem::path default_path()
+static const fs::path default_path()
 {
-    std::experimental::filesystem::path config_path;
+    fs::path config_path;
     char* home_env_variable = getenv("XDG_DATA_HOME");
 
     if (home_env_variable != nullptr) {
@@ -45,7 +47,7 @@ static const std::experimental::filesystem::path default_path()
         if (home_env_variable == nullptr) {
             std::cerr << rang::fg::red << "Error: Could not locate HOME directory. This is likely not your fault.\n"
                          "To fix this problem, try defining your own config path as the environment variable DLOG_PATH." << rang::fg::reset << std::endl;
-        return std::experimental::filesystem::path();
+        return fs::path();
         }
 
         config_path = home_env_variable;
@@ -54,30 +56,30 @@ static const std::experimental::filesystem::path default_path()
     }
 
     std::error_code error;
-    if (!std::experimental::filesystem::create_directories(config_path, error) && error) {
+    if (!fs::create_directories(config_path, error) && error) {
         std::cerr << rang::fg::red << "Error: Could not create config directory.\n"
                      "File system reported: " << error.message() << " (" << error.value() << ")" << rang::fg::reset << std::endl;
-        return std::experimental::filesystem::path();
+        return fs::path();
     }
 
     return config_path;
 }
 #endif
 
-const std::experimental::filesystem::path& files::dlog_dir()
+const fs::path& files::dlog_dir()
 {
     if (!m_dlog_dir.has_value()) {
         const auto path = getenv(static_cast<const char*>(ENV_VARIABLE));
 
         if (path != nullptr) {
-            const std::experimental::filesystem::path custom_path(path);
+            const fs::path custom_path(path);
 
-            if (std::experimental::filesystem::exists(custom_path)) {
-                m_dlog_dir = std::experimental::filesystem::path(path);
+            if (fs::exists(custom_path)) {
+                m_dlog_dir = fs::path(path);
             } else {
                 std::cerr << rang::fg::red << "Error: Invalid DLOG_PATH [" << path << "].\n"
                              "Check your DLOG_PATH environment variable and make sure it's a valid directory." << rang::fg::reset << std::endl;
-                m_dlog_dir = std::experimental::filesystem::path();
+                m_dlog_dir = fs::path();
             }
         } else {
             m_dlog_dir = default_path();
@@ -87,7 +89,7 @@ const std::experimental::filesystem::path& files::dlog_dir()
     return *m_dlog_dir;
 }
 
-const std::experimental::filesystem::path files::dlog_file(const std::string& file_name)
+const fs::path files::dlog_file(const std::string& file_name)
 {
     const auto& directory = files::dlog_dir();
 
@@ -95,8 +97,42 @@ const std::experimental::filesystem::path files::dlog_file(const std::string& fi
         return directory;
     }
 
-    auto file = std::experimental::filesystem::path(directory);
+    auto file = fs::path(directory);
     file.append(file_name);
 
     return file;
+}
+
+bool files::prepare_for_write(const fs::path& path)
+{
+    if (!fs::exists(path)) {
+        return true;
+    }
+
+    fs::path tmp(path);
+    tmp.append(".tmp");
+
+    return fs::copy_file(path, tmp, fs::copy_options::update_existing);
+}
+
+void files::accept_changes(const fs::path& path)
+{
+    fs::path tmp(path);
+    tmp.append(".tmp");
+
+    fs::remove(tmp);
+}
+
+void files::restore(const fs::path& path)
+{
+    fs::path tmp(path);
+    tmp.append(".tmp");
+
+    if (fs::exists(tmp)) {
+        fs::copy_file(tmp, path, fs::copy_options::overwrite_existing);
+    } else {
+        // No .tmp file means the file did not exist prior to the write
+        // so we can just delete it to go back to the previous state of the file system.
+        fs::remove(path);
+    }
 }
--- src/files.h
+++ src/files.h
@@ -19,4 +19,22 @@ namespace files {
      * If no directory for dlog can be determined, this function returns an empty path.
      */
     const std::experimental::filesystem::path dlog_file(const std::string& file_name);
+
+    /**
+     * Creates a temporary (.tmp) file with the same contents as the file specified by path
+     * and returns true if successfully copied.
+     *
+     * Note that this function returns true even if the file does not exist. In that case,
+     * no .tmp file is created and a restore() simply deletes the newly created file.
+     *
+     * If this function returns false, this means that the file already existed but no .tmp
+     * file could be created for it. In that case, you should always abort subsequent writes.
+     */
+    bool prepare_for_write(const std::experimental::filesystem::path& path);
+
+    /** Restores the file specified by path from the .tmp file created by backup(). */
+    void restore(const std::experimental::filesystem::path& path);
+
+    /** Accepts changes made to a file previously prepared for write by removing the associated .tmp file. */
+    void accept_changes(const std::experimental::filesystem::path& path);
 }
