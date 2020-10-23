a3e9ba4d62ca4148a7e4825fbed368adc55f5bf7
cengels <cengels@tuta.io>
2020-10-23 15:23:13 +0200

    Disable multi-word subcommands
    This is to ensure positional arguments are parsed correctly and passed
    into the subcommand instead of being assumed to be part of the
    subcommand.

--- src/commandline.cpp
+++ src/commandline.cpp
@@ -15,11 +15,7 @@ static std::string parse_subcommand(int& arg_count, char **& args)
 {
     std::string subcommand = std::string();
 
-    for (int i = 1; i < arg_count && args[i][0] != '-'; i++) {
-        if (!subcommand.empty()) {
-            subcommand += " ";
-        }
-
+    for (int i = 1; i < arg_count && args[i][0] != '-' && subcommand.empty(); i++) {
         subcommand += args[i];
         // "delete" array element by copying it to the end and decrementing arg_count
         std::copy(args+i+1, args+arg_count, args+i);
