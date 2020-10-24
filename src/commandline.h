81483f3cfbf198c13ad4a93c3ea887ecd51ed409
cengels <cengels@tuta.io>
2020-10-24 15:25:23 +0200

    Add command_line::confirm() method (untested)

--- src/commandline.h
+++ src/commandline.h
@@ -4,6 +4,8 @@
  * Deals with the display and processing of command line arguments.
  */
 namespace command_line {
-    /// Parses the given command line arguments.
+    /** Parses the given command line arguments. */
     int parse(int& arg_count, char **&args);
+    /** Asks the user for confirmation and returns their response. */
+    bool confirm(const char* prompt);
 }
